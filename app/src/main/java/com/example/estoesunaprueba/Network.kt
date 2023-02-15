8e4c7a7ddb8c10b0cf08fee61dd9461e284a80d4
Sito8943 <sito8943@gmail.com>
2023-02-15 13:04:47 -0800

    http routes

--- app/src/main/java/com/example/estoesunaprueba/Network.kt
+++ app/src/main/java/com/example/estoesunaprueba/Network.kt
@@ -1,6 +1,7 @@
 package com.example.estoesunaprueba
 
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Call
@@ -19,21 +20,154 @@ object ServiceBuilder {
         .client(client)
         .build()
 
-    fun<T> buildService(service: Class<T>): T{
+    fun <T> buildService(service: Class<T>): T {
         return retrofit.create(service)
     }
 }
 
 interface ApiInterface {
 
-    @POST("getstring")
-    fun sendReq(@Body requestModel: RequestModel) : Call<ResponseModel>
+    /**
+     * request {
+     *  idcliente: String
+     *  nombre: String
+     *  ci: String
+     *  longitudo: Double
+     *  latitudo: Double
+     *  longitudd: Double
+     *  latitudd: Double
+     *  cantidad: Integer
+     *  tipotaxi: Integer
+     * }
+     * response {
+     *  choferes: List<Chofer>
+     *  error: String
+     * }
+     */
+    @POST("getsolicitataxi")
+    fun getSolicitudTaxis(@Body requestModel: RequestSolicitudTaxis): Call<ResponseSolicitudTaxis>
+
+    /**
+     * request {
+     *  idcliente: String
+     *  idchofer: String
+     * }
+     * response {
+     *  msg: String
+     * }
+     */
+    @POST("geteligechofer")
+    fun getEligeChofer(@Body requestModel: RequestEligeChofer): Call<ResponseEligeChofer>
+
+    /**
+     * request {
+     *  idchofer: String
+     *  idcliente: String
+     * }
+     * response {
+     *  error: String
+     * }
+     */
+    @POST("getcarreraok")
+    fun getCarreraOk(@Body requestModel: RequestCarreraOk): Call<ResponseCarreraOk>
+
+    /**
+     * request
+     * body {
+     *  idcliente: String
+     *  password: String
+     *  longitud: Double
+     *  latitud: Double
+     * }
+     * response
+     * body {
+     *  error: String
+     * }
+     */
+    @POST("getlogincliente")
+    fun getLoginCliente(@Body requestModel: RequestLoginCliente): Call<ResponseLoginCliente>
+
+    /**
+     * request
+     * body {
+     *  idchofer: String
+     *  password: String
+     *  longitud: Double
+     *  latitud: Double
+     * }
+     * response
+     * body {
+     *  error: String
+     * }
+     */
+    @POST("getloginchofer")
+    fun getLoginChofer(@Body request: RequestLoginChofer): Call<ResponseLoginChofer>
+
+
 }
 
-data class ResponseModel(
-    val error: String
+// requests
+data class RequestSolicitudTaxis(
+    val idcliente: String,
+    val nombre: String,
+    val ci: String,
+    val longitud: Double,
+    val latitud: Double,
+    val cantidad: Integer,
+    val tipotaxi: Integer
 )
 
-data class RequestModel(
-    val mensaje: String,
+data class RequestLoginChofer(
+    val idchofer: String,
+    val password: String,
+    val longitud: Double,
+    val latitud: Double,
+)
+
+data class RequestEligeChofer(
+    val idcliente: String,
+    val idchofer: String
+)
+
+data class RequestCarreraOk(
+    val idchofer: String,
+    val idcliente: String
+)
+
+data class RequestLoginCliente(
+    val idcliente: String,
+    val password: String,
+    val longitud: Double,
+    val latitud: Double,
+)
+
+data class Chofer(
+    val idchofer: String,
+    val longitud: Double,
+    val latitud: Double,
+    val ci: String,
+    val phone: String,
+    val tarifa: Integer,
+)
+
+// responses
+data class ResponseSolicitudTaxis(
+    val choferes: List<Chofer>,
+    val error: String? = null
+)
+
+data class ResponseLoginChofer(
+    val error: String? = null
+)
+
+data class ResponseEligeChofer(
+    val msg: String
+)
+
+data class ResponseCarreraOk(
+    val error: String? = null
+)
+
+data class ResponseLoginCliente(
+    val error: String? = null
 )
